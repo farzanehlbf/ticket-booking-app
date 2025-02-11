d6e893cf385b6e69d1d30b1874ea426f97e10258
farzaneh labafi <85051697+farzanehlbf@users.noreply.github.com>
2025-02-11 09:50:51 +0330

    000-00-01 create models and migrations

---
+++ database/migrations/2025_02_11_060833_create_trips_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('origin_id')->constrained()->onDelete('cascade'); // ارتباط با مبدا
+            $table->foreignId('destination_id')->constrained()->onDelete('cascade'); // ارتباط با مقصد
+            $table->foreignId('terminal_id')->constrained()->onDelete('cascade'); // ارتباط با ترمینال
+            $table->timestamp('departure_time'); // زمان حرکت
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
---
+++ database/migrations/2025_02_11_060903_create_tickets_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reservation_passenger_id')->constrained()->onDelete('cascade'); // ارتباط با مسافر رزرو
+            $table->string('ticket_number')->unique(); // شماره بلیط
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
